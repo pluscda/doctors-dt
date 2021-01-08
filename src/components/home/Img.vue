8356eaf969e1e9f408dd4d6a1b4507b4bc73eb6e
alex <pluscda@gmail.com>
2021-01-08 17:24:52 +0800

    g

--- src/components/home/Img.vue
+++ src/components/home/Img.vue
@@ -5,10 +5,9 @@
 <script>
 import { Doughnut, mixins } from "vue-chartjs";
 const { reactiveData } = mixins;
-const types = ["分常備役", "替代役", "免役"];
+const types = ["已完成", "未完成", "待處理"];
 // #2
-const PRE_URL =
-  "/reportStatistics/ImageQuality?startDate={%0}&endDate={%1}&$inlinecount=allpages&$skip=0&$top=11111440";
+const PRE_URL = "/reportStatistics/ImageQuality?startDate={%0}&endDate={%1}&$inlinecount=allpages&$skip=0&$top=11111440";
 export default {
   labels: types,
   extends: Doughnut,
@@ -20,8 +19,8 @@ export default {
       chartdata: {},
       options: {
         responsive: true,
-        maintainAspectRatio: false
-      }
+        maintainAspectRatio: false,
+      },
     };
   },
   methods: {
@@ -31,25 +30,19 @@ export default {
 
     async getData(num = 1) {
       this.$root.$emit("影像品質", true);
-      let imgs = await window.axios.get(
-        "/GeneralData/SelectNoList?groupNo=ImageQuality"
-      );
+      let imgs = await window.axios.get("/GeneralData/SelectNoList?groupNo=ImageQuality");
       let arr = [];
       const today = window.dtcToday;
       const start = window.dtcStart(num);
       imgs = imgs.Items;
-      imgs.forEach(s => {
+      imgs.forEach((s) => {
         arr.push(s.Name);
       });
       this.labels = arr;
       const url = PRE_URL.replace(/{%0}/, start).replace(/{%1}/, today);
       try {
         const map = await window.axios.get(url);
-        const data = [
-          map[0].IMAGEQUALITY30_COUNT,
-          map[0].IMAGEQUALITY40_COUNT,
-          map[0].IMAGEQUALITY60_COUNT
-        ];
+        const data = [map[0].IMAGEQUALITY30_COUNT, map[0].IMAGEQUALITY40_COUNT, map[0].IMAGEQUALITY60_COUNT];
         this.$root.$emit("影像品質", false);
         this.drawReport(data);
       } catch (e) {
@@ -59,35 +52,27 @@ export default {
       //this.drawReport(data);
     },
     drawReport(data) {
-      const labels = ["常備役", "替代役", "免役"]; // this.labels;
+      const labels = ["已完成", "未完成", "待處理"];
       this.chartData = {
-        labels: [
-          `${labels[0]}(${data[0]})`,
-          `${labels[1]}(${data[1]})`,
-          `${labels[2]}(${data[2]})`
-        ],
+        labels: [`${labels[0]}(${data[0]})`, `${labels[1]}(${data[1]})`, `${labels[2]}(${data[2]})`],
         datasets: [
           {
             label: "體位",
             backgroundColor: ["#41B883", "#E46651", "#ffc107"],
-            data
-          }
-        ]
+            data,
+          },
+        ],
       };
-    }
+    },
   },
   async mounted() {
-    this.drawReport([
-      this.getRandomInt(),
-      this.getRandomInt(),
-      this.getRandomInt()
-    ]);
+    this.drawReport([this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]);
   },
   watch: {
     time(val) {
       this.getData(val);
-    }
-  }
+    },
+  },
 };
 </script>
 

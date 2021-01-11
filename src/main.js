33ef90b3f8877c34c08fc70159c0522dad0304c7
alex <pluscda@gmail.com>
2021-01-11 10:34:45 +0800

    g

--- src/main.js
+++ src/main.js
@@ -33,9 +33,10 @@ Vue.prototype.$formatPrice = function(value) {
 
 Vue.prototype.$twDate = function(str){
   if(!str || str.startsWith("0")) return "暫無資料";
-  const twYear = +str.split("T")[0].split("-")[0] - 1911;
-  const d = new Date(str.split("T")[0]);
-  return twYear + "年" + moment(str).format("MM月DD日 HH:mm");
+  return moment(str).format('YYYY-MM-DD HH:mm:ss');
+  // const twYear = +str.split("T")[0].split("-")[0] - 1911;
+  // const d = new Date(str.split("T")[0]);
+  // return twYear + "年" + moment(str).format("MM月DD日 HH:mm");
 }
 
 Vue.prototype.$formatStatus = function(str){

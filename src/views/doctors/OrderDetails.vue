3fc536fe77b748e09ea8b07cf1b834c348af636e
alex <pluscda@gmail.com>
2021-02-25 16:28:19 +0800

    g

--- src/views/doctors/OrderDetails.vue
+++ src/views/doctors/OrderDetails.vue
@@ -54,7 +54,7 @@
             :readonly="isAdmin"
             v-model="item.judge"
             autofocus
-            :placeholder="isAdmin ? '請在此輸入報告(NOTE: Admin is not allowed to type here)...' : '請在此輸入報告...'"
+            :placeholder="isAdmin ? 'Admin is not allowed to type here' : '請在此輸入報告...'"
             spellcheck="false"
             no-resize
             class="input-area-dtc"
